82a54fc620844dc1519c350c678cc0ea24e075b2
techcatnip <jiurixue@outlook.com>
2024-01-17 15:10:57 +0800

    Site updated: 2024-01-17 15:10:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4a17b156.html","posts/7c4b9639.html","posts/dfa35092.html"];function toRandomPost(){
+var posts=["posts/4a17b156.html","posts/dfa35092.html","posts/7c4b9639.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

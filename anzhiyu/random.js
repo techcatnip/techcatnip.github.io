bab722bf867ab2ad5b724899715789eeb7fd43b5
techcatnip <jiurixue@outlook.com>
2024-01-19 18:36:57 +0800

    Site updated: 2024-01-19 18:36:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/217c81a9.html","posts/ec80583f.html","posts/7c4b9639.html","posts/4a17b156.html","posts/dfa35092.html"];function toRandomPost(){
+var posts=["posts/217c81a9.html","posts/ec80583f.html","posts/4a17b156.html","posts/dfa35092.html","posts/7c4b9639.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
